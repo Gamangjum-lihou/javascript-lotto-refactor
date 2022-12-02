de9e6ce2f4956c38249cd433cdefd30e1916ac0b
SWARVY <swarvy0826@naver.com>
2022-12-02 14:28:04 +0900

    feat : 컨트롤러에서 구매금액 입력, 생성된 로또 출력, 당첨번호 입력 구현

--- src/Controller/LottoController.js
+++ src/Controller/LottoController.js
@@ -1,3 +1,43 @@
-class LottoController {}
+const Lotto = require('../Lotto');
+const Player = require('../Player');
+const { readLottoPrice, readWinLottoMainNumber, readWinLottoBonusNumber } = require('../View/InputView');
+const { printCreatedLottoNumber, printResult, printMargin } = require('../View/OutputView');
+
+class LottoController {
+  #Lotto;
+
+  #Player;
+
+  constructor() {
+    this.#Player = new Player();
+  }
+
+  getLottoPrice = () => {
+    readLottoPrice(this.sendLottoPrice);
+  };
+
+  sendLottoPrice = (price) => {
+    this.#Player.storeMoney(price);
+    this.printPlayerLottery();
+  };
+
+  printPlayerLottery = () => {
+    const playerLottery = this.#Player.getPlayerLottery();
+    printCreatedLottoNumber(playerLottery);
+    this.getLottoMainNumber();
+  };
+
+  getLottoMainNumber = () => {
+    readWinLottoMainNumber(this.getLottoBonusNumber);
+  };
+
+  getLottoBonusNumber = (mainNumber) => {
+    readWinLottoBonusNumber(mainNumber, this.sendWinLottery);
+  };
+
+  sendWinLottery = (mainNumber, bonusNumber) => {
+    this.#Lotto = new Lotto(mainNumber, bonusNumber);
+  };
+}
 
 module.exports = LottoController;

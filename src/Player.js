f6e6994960b555e76ee84a983e389095c5446d61
SWARVY <swarvy0826@naver.com>
2022-12-02 14:23:13 +0900

    feat : Player 기능 구현 (구매 금액 저장, 로또 생성, 생성된 로또 반환)

--- src/Player.js
+++ src/Player.js
@@ -1,3 +1,25 @@
-class Player {}
+const { Random } = require('@woowacourse/mission-utils');
+
+class Player {
+  #money;
+
+  #numbers;
+
+  storeMoney(money) {
+    this.#money = money;
+    this.#numbers = this.generateLottery();
+  }
+
+  generateLottery() {
+    Array.from({ length: this.#money / 1000 }, () => {
+      const numbers = Random.pickUniqueNumbersInRange(1, 45, 6);
+      return numbers.sort();
+    });
+  }
+
+  getPlayerLottery() {
+    return this.#numbers;
+  }
+}
 
 module.exports = Player;

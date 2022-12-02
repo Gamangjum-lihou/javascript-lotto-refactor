f42e2be8ae4b1162e47090e41e686b9516faa350
SWARVY <swarvy0826@naver.com>
2022-12-02 13:54:51 +0900

    feat : inputView에서 Console을 통해 값을 받아오는 기능 구현

--- src/View/InputView.js
+++ src/View/InputView.js
@@ -1,3 +1,23 @@
-const InputView = {};
+const { Console } = require('@woowacourse/mission-utils');
+
+const InputView = {
+  readLottoPrice(callback) {
+    Console.readLine('', (value) => {
+      callback(value);
+    });
+  },
+
+  readWinLottoMainNumber(callback) {
+    Console.readLine('', (value) => {
+      callback(value);
+    });
+  },
+
+  readWinLottoBonusNumber(callback) {
+    Console.readLine('', (value) => {
+      callback(value);
+    });
+  },
+};
 
 module.exports = InputView;

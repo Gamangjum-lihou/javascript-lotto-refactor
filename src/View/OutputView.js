d2bc6c958181fd42717bdcb07867639bf7b8bbc1
SWARVY <swarvy0826@naver.com>
2022-12-02 15:08:47 +0900

    feat : OutputView 초안 구현

--- src/View/OutputView.js
+++ src/View/OutputView.js
@@ -1,3 +1,20 @@
-const OutputView = {};
+const { Console } = require('@woowacourse/mission-utils');
+
+const OutputView = {
+  printCreatedLotteryNumber(lottery, money) {
+    Console.print(money);
+    Console.print(lottery);
+  },
+
+  printResult(result) {
+    Console.print(result);
+  },
+
+  printMargin() {},
+
+  exit() {
+    return Console.close();
+  },
+};
 
 module.exports = OutputView;

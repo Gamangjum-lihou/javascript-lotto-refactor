90ef40e8d0679eae8c173b9837c5f14f37e6f60c
SWARVY <swarvy0826@naver.com>
2022-12-03 16:52:17 +0900

    refactor : 자주 사용하는 상수 추가 분리

--- src/Constants.js
+++ src/Constants.js
@@ -1,12 +1,22 @@
-const INDEX = {
+const INDEX = Object.freeze({
   first: 0,
   second: 1,
   third: 2,
   fourth: 3,
   fifth: 4,
   fail: 5,
+});
+
+const INFO = {
+  lotto_start: 1,
+  lotto_end: 45,
+  lotto_size: 6,
 };
+
+const UNIT = 1000;
 
 module.exports = {
   INDEX,
+  UNIT,
+  INFO,
 };

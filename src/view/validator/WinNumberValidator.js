de26ae8131c451ce2e042956abe61a8ec80ae814
y00eunji <aine0227@naver.com>
2022-12-06 17:22:10 +0900

    feat: Add win number validator

--- src/view/validator/WinNumberValidator.js
+++ src/view/validator/WinNumberValidator.js
@@ -1,18 +1,11 @@
 const ValidationError = require('../../error/ValidationError');
 const { ERROR } = require('../../utils/constants');
+const { isNumber } = require('./NumberValidator');
 
 const WinNumberValidator = {
-  isNumber(input) {
-    const inputNumber = Math.floor(Number(input));
-
-    return String(inputNumber) === input && inputNumber >= 0;
-  },
-
   validate(input) {
     input.split(',').forEach((num) => {
-      if (!WinNumberValidator.isNumber(num)) {
-        throw new ValidationError(ERROR.win_number);
-      }
+      if (!isNumber(num)) throw new ValidationError(ERROR.win_number);
     });
   },
 };

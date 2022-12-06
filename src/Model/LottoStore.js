4ae91f324f931a7b370cc9cd701d829823300467
Leemainsw <s2018s15@e-mirim.hs.kr>
2022-12-06 23:06:43 +0900

    :sparkles: feat : 입력받는 값들에 대한 모델 정의 (로또 번호, 보너스, 가격)

---
+++ src/Model/LottoStore.js
@@ -0,0 +1,54 @@
+const { MESSAGE } = require('../lib/constants/error');
+const { PRICE_UNIT } = require('../lib/constants/system');
+const { lottoListFormatter } = require('../lib/utils/formatter');
+const { createRandomLotto } = require('../lib/utils/gameSystem');
+const {
+  isNumber, isUnitNumber,
+} = require('../lib/utils/validation');
+const ValidationError = require('../ValidationError');
+
+class Account {
+  #account;
+
+  #count;
+
+  #lottos;
+
+  constructor(account) {
+    this.#account = Number(account);
+    this.validate();
+    this.calc();
+  }
+
+  validate() {
+    if (!isNumber(this.#account)) {
+      throw new ValidationError(MESSAGE.number);
+    }
+    if (!isUnitNumber(this.#account)) {
+      throw new ValidationError(MESSAGE.unit);
+    }
+  }
+
+  calc() {
+    this.#count = this.#account / PRICE_UNIT;
+    this.#lottos = createRandomLotto(this.#count);
+  }
+
+  getAccount() {
+    return this.#account;
+  }
+
+  getCount() {
+    return this.#count;
+  }
+
+  getLottos() {
+    return this.#lottos;
+  }
+
+  getLottosMessage() {
+    return this.#lottos.map((lotto) => lottoListFormatter(lotto));
+  }
+}
+
+module.exports = Account;

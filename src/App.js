1c3020150606cb41f59aab76a487320bc4a99042
Leemainsw <s2018s15@e-mirim.hs.kr>
2022-12-06 23:08:59 +0900

    :sparkles: feat : 전체적인 흐름도 App.js에 정의

--- src/App.js
+++ src/App.js
@@ -1,5 +1,102 @@
+const { INPUT_MESSAGE, LINE_BREAK } = require('./lib/constants/message');
+const LottoStore = require('./Model/LottoStore');
+const Lotto = require('./Model/Lotto');
+const Bonus = require('./Model/Bonus');
+const { MIN_MATCH_COUNT } = require('./lib/constants/system');
+const { resultMessageFormmatter } = require('./lib/utils/formatter');
+const View = require('./View');
+const { getRate } = require('./lib/utils/gameSystem');
+
 class App {
-  play() {}
+  lottoStore;
+
+  lotto;
+
+  bonus;
+
+  constructor() {
+    this.view = new View();
+  }
+
+  play() {
+    this.inputPrice();
+  }
+
+  inputPrice() {
+    const handler = (input) => {
+      try {
+        this.lottoStore = new LottoStore(input);
+        this.printLottoCountAndList();
+        this.inputLotto();
+      } catch (err) {
+        this.errorHandler(err, this.inputPrice.bind(this));
+      }
+    };
+    this.view.input.readLine(INPUT_MESSAGE.price, handler);
+  }
+
+  printLottoCountAndList() {
+    this.view.output.printLottoCount(this.lottoStore.getCount());
+    this.view.output.printLottoList(this.lottoStore.getLottosMessage());
+  }
+
+  inputLotto() {
+    const handler = (input) => {
+      try {
+        this.lotto = new Lotto(input);
+        this.inputBonus();
+      } catch (err) {
+        this.errorHandler(err, this.inputLotto.bind(this));
+      }
+    };
+    this.view.input.readLine(LINE_BREAK + INPUT_MESSAGE.lotto, handler);
+  }
+
+  inputBonus() {
+    const handler = (input) => {
+      try {
+        this.bonus = new Bonus(input);
+        this.printResult();
+      } catch (err) {
+        this.errorHandler(err, this.inputBonus.bind(this));
+      }
+    };
+    this.view.input.readLine(LINE_BREAK + INPUT_MESSAGE.bonus, handler);
+  }
+
+  errorHandler(error, callback) {
+    this.view.output.print(error.message + LINE_BREAK);
+    callback();
+  }
+
+  printResult() {
+    const point = this.getPoint();
+    const rate = getRate(point, this.lottoStore.getAccount());
+    this.view.output.print(resultMessageFormmatter(point, rate));
+  }
+
+  getPoint() {
+    const lottoList = this.lottoStore.getLottos();
+    const winNumber = this.lotto.getLotto();
+    const bonus = this.bonus.getBonus();
+
+    return lottoList.reduce(
+      (acc, lotto) => {
+        let matchCount = lotto.filter((num) => winNumber.includes(String(num))).length;
+        if (matchCount === 5 && lotto.includes(bonus)) {
+          matchCount += 1;
+        }
+        if (matchCount === 6) {
+          matchCount += 1;
+        }
+        if (matchCount >= MIN_MATCH_COUNT) {
+          acc[matchCount - MIN_MATCH_COUNT] += 1;
+        }
+        return acc;
+      },
+      [0, 0, 0, 0, 0],
+    );
+  }
 }
 
 module.exports = App;

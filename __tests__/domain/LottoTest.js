f55eaf0075b3750e8c6bc70cea09e753e7719abb
y00eunji <aine0227@naver.com>
2022-12-06 17:26:54 +0900

    test: Test lotto class

--- __tests__/domain/LottoTest.js
+++ __tests__/domain/LottoTest.js
@@ -14,4 +14,18 @@ describe('Lotto 클래스 검사', () => {
     const lotto = new Lotto([1, 5, 6, 7, 8, 9]);
     expect(lotto.getLotto()).toEqual([1, 5, 6, 7, 8, 9]);
   });
+
+  test('두 로또의 중복된 숫자의 개수를 반환한다.', () => {
+    const lotto1 = new Lotto([1, 2, 3, 4, 5, 6]);
+    const lotto2 = new Lotto([1, 2, 3, 4, 5, 6]);
+
+    expect(lotto1.getDuplicate(lotto2)).toEqual(6);
+  });
+
+  test('로또에 특정 번호의 유무를 리턴한다.', () => {
+    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+
+    expect(lotto.hasNumber(1)).toBeTruthy();
+    expect(lotto.hasNumber(8)).toBeFalsy();
+  });
 });
